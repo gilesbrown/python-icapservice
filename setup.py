96871ab62c6635c396325591c84bed243745fd16
Giles Brown <gsbrown@ebay.com>
2017-04-14 13:46:38 -0700

    Add support for brotli content encoding and alias for none

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='icapservice',
-    version='0.1.1',
+    version='0.2.0',
     description='ICAP service library for Python',
     author='Giles Brown',
     author_email='giles_brown@hotmail.com',
